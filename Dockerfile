34ac4d702cfb697e8897956663edb2c6df3c9728
Mike Gore <mikegore1000@msn.com>
2022-10-05 22:40:54 +0100

    Host GreetingApi in a container for testing

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM postgres:15rc1-alpine
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
 
-# See docs on initalization scripts here https://hub.docker.com/_/postgres
-ADD init.sql /docker-entrypoint-initdb.d/
+WORKDIR /app
+
+COPY ./ ./
+RUN dotnet publish -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/aspnet:6.0
+WORKDIR /app
+COPY --from=build-env /app/out .
+ENTRYPOINT ["dotnet", "GreetingApi.dll"]
--- GreetingApi.Tests/GreetingApiTests.cs
+++ GreetingApi.Tests/GreetingApiTests.cs
@@ -1,24 +1,17 @@
 using System;
-using System.Net.Http;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 using PactNet.Verifier;
 using Xunit.Abstractions;
 using PactNet.Infrastructure.Outputters;
-using Microsoft.AspNetCore.Builder;
-using System.Threading.Tasks;
-using System.Threading;
 
 namespace GreetingApi.Tests;
 
-public class GreetingApiTests : IClassFixture<GreetingApiFixture>
+public class GreetingApiTests
 {
-    private readonly GreetingApiFixture fixture;
     private readonly ITestOutputHelper output;
 
-    public GreetingApiTests(GreetingApiFixture fixture, ITestOutputHelper output)
+    public GreetingApiTests(ITestOutputHelper output)
     {
-        this.fixture = fixture;
         this.output = output;
     }
 
@@ -51,23 +44,5 @@ public class XUnitOutput : IOutput
     public void WriteLine(string line)
     {
         this.output.WriteLine(line);
-    }
-}
-
-public class GreetingApiFixture : IAsyncDisposable
-{
-    private WebApplication application;
-
-    public GreetingApiFixture()
-    {
-        var builder = WebApplication.CreateBuilder(Array.Empty<string>());
-        application = Program.BuildApplication(builder);
-        application.RunAsync("http://localhost:5001");
-        Thread.Sleep(TimeSpan.FromSeconds(10)); // Handle this better!
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        await application.DisposeAsync();
     }
 }
