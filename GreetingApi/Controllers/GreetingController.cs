cd719f0b52ede11e26405d1b6852ec3abc1cc464
mike-gore-cko <mike.gore@checkout.com>
2022-11-02 05:57:25 +0000

    Tidied up API

--- GreetingApi/Controllers/GreetingController.cs
+++ GreetingApi/Controllers/GreetingController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Serialization;
 
 namespace GreetingApi.Controllers;
 
@@ -7,24 +6,11 @@ namespace GreetingApi.Controllers;
 [Route("[controller]")]
 public class GreetingController : ControllerBase
 {
-    private readonly ILogger<GreetingController> _logger;
-
-    public GreetingController(ILogger<GreetingController> logger)
-    {
-        _logger = logger;
-    }
-
     [HttpGet(Name = "GetGreeting")]
     public GetGreetingResponse Get()
     {
-        return new GetGreetingResponse {
-            Greeting = "Hello World"
-        };
+        return new GetGreetingResponse("Hello World");
     }
 }
 
-public class GetGreetingResponse
-{
-    [JsonPropertyName("greeting")]
-    public string? Greeting {get;set;}
-}
+public record GetGreetingResponse(string Greeting);
--- GreetingApi/Program.cs
+++ GreetingApi/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 BuildApplication(builder).Run();
 
@@ -5,7 +7,9 @@ BuildApplication(builder).Run();
 public partial class Program {
     public static WebApplication BuildApplication(WebApplicationBuilder builder) {
         // Add services to the container.
-        builder.Services.AddControllers();
+        builder.Services.AddControllers()
+            .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
+        
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
