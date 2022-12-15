ec0af72a42e2030a92e349f0e4bc0c0abc36bf8c
Mike Gore <mike.gore@checkout.com>
2022-12-15 13:49:58 +0000

    Add variables to can-i-deploy

--- can-i-deploy.sh
+++ can-i-deploy.sh
@@ -1,13 +1,11 @@
-#!/bin/bash  
-#dotnet test Consumer.Tests
-#
+#!/bin/bash
 export PACT_BROKER_BASE_URL="http://localhost:9292"
 export PACT_BROKER_USERNAME=""
 export PACT_BROKER_PASSWORD=""
-#
-#version=1.0.0
-#git_hash=$(git rev-parse HEAD)
-#git_branch=$(git rev-parse --abbrev-ref HEAD)
+
+version=1.0.0
+environment=production
+participant=Consumer
 
 # See https://docs.pact.io/pact_broker/can_i_deploy
 docker run --rm \
@@ -20,6 +18,6 @@ docker run --rm \
     pactfoundation/pact-cli:latest \
     pact-broker \
     can-i-deploy \
-    --pacticipant "Consumer" \
-    --version "1.0.0" \
-    --to-environment production
+    --pacticipant "$participant" \
+    --version "$version" \
+    --to-environment "$environment"
